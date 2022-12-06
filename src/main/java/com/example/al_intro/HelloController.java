fa5e62025022edb3646f127ca6d4c6d28486823f
James <james.bidule@gmail.com>
2022-12-06 17:31:58 +0100

    chore: updated welcome message

--- src/main/java/com/example/al_intro/HelloController.java
+++ src/main/java/com/example/al_intro/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.al_intro;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button welcomeButton;
+
+    @FXML
+    private TextField nameInput;
+
+    @FXML
+    private ImageView welcomeGif;
+
+    private static final String welcomeMessage = "Welcome to JavaFX Application %s!";
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("");
+        welcomeGif.setVisible(false);
+
+        if(nameInput.getText().isEmpty()) {
+            welcomeText.setText(String.format(welcomeMessage, ""));
+        } else {
+            welcomeText.setText(String.format(welcomeMessage, ", " + nameInput.getText()));
+            welcomeGif.setVisible(true);
+            System.out.println("Hello " + nameInput.getText());
+        }
+
     }
+
 }
